efb42fdeaafa781469b72669ca6f7b835b9a917a
NicolasNewman <nnewman316@gmail.com>
2023-05-26 13:19:43 +0900

    updated router paths

--- packages/site/src/router.tsx
+++ packages/site/src/router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from 'react-router-dom';
 import Root from './pages/Root';
 import Home from './pages/Home';
 import Testing from './pages/Testing';
+import NGramExplorer from './pages/NGramExplorer';
+import Overview from './pages/Overview';
 
 export default createBrowserRouter([
 	{
@@ -11,6 +13,14 @@ export default createBrowserRouter([
 			{
 				path: 'home',
 				element: <Home />,
+			},
+			{
+				path: 'overview',
+				element: <Overview />,
+			},
+			{
+				path: 'ngrams',
+				element: <NGramExplorer />,
 			},
 			{
 				path: 'testing',
