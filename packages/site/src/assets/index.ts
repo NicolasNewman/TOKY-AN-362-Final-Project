5d9ccd35086a2081912c3883533486ec8c44d6ff
NicolasNewman <nnewman316@gmail.com>
2023-05-27 15:23:48 +0900

    added movie posters

---
+++ packages/site/src/assets/index.ts
@@ -0,0 +1,34 @@
+import { MovieId } from '@ghibli-analysis/shared/dist/web';
+import m148901 from './148901.jpg';
+import m149868 from './149868.jpg';
+import m150435 from './150435.jpg';
+import m150436 from './150436.jpg';
+import m151441 from './151441.jpg';
+import m152271 from './152271.jpg';
+import m159561 from './159561.jpg';
+import m161722 from './161722.jpg';
+import m163027 from './163027.jpg';
+import m240799 from './240799.jpg';
+import m327529 from './327529.jpg';
+import m335800 from './335800.jpg';
+import m344584 from './344584.jpg';
+import m89778 from './89778.jpg';
+import m89972 from './89972.jpg';
+
+export const posters: { [key in MovieId]: string } = {
+	148901: m148901,
+	149868: m149868,
+	150435: m150435,
+	150436: m150436,
+	151441: m151441,
+	152271: m152271,
+	159561: m159561,
+	161722: m161722,
+	163027: m163027,
+	240799: m240799,
+	327529: m327529,
+	335800: m335800,
+	344584: m344584,
+	89778: m89778,
+	89972: m89972,
+};
