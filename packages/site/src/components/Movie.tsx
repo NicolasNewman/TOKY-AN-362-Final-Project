8aa3430099cb213289513481ff97db2157d74217
NicolasNewman <nnewman316@gmail.com>
2023-05-27 15:24:21 +0900

    proper styling of overview page

--- packages/site/src/components/Movie.tsx
+++ packages/site/src/components/Movie.tsx
@@ -1,7 +1,10 @@
 import { Data, movieIdToName } from '@ghibli-analysis/shared/dist/web';
-import React from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 import { round } from '../lib/math';
 import { Card } from 'antd';
+import { posters } from '../assets';
+import { StarOutlined } from '@ant-design/icons';
+import Stat from './Stat';
 
 interface IProps {
 	movie: Data;
@@ -11,19 +14,43 @@ const Movie: React.FC<IProps> = ({ movie }) => {
 	const movieName = movieIdToName[movie.movieId];
 	const { n, avg, nPositive, nNegative, nMixed, avgStrong } = movie.stats;
 	return (
-		<Card title={movieName}>
-			<div className="flex flex-col">
-				<div>Reviews: {n}</div>
-				<div>Average: {avg}</div>
-				<div>Strong Average: {avgStrong}</div>
-				<div>
-					Positive: {nPositive} ({round((nPositive / n) * 100, 2)}%)
-				</div>
-				<div>
-					Negative: {nNegative} ({round((nNegative / n) * 100, 2)}%)
-				</div>
-				<div>
-					Mixed: {nMixed} ({round((nMixed / n) * 100, 2)}%)
+		<Card className="h-fit" title={movieName}>
+			<div className="flex">
+				<img className="h-[128px]" src={posters[movie.movieId]} alt={movieIdToName[movie.movieId]} />
+				<div className="flex flex-col p-2 w-full">
+					<Stat k="Reviews" v={n} />
+					<Stat
+						k="Average"
+						v={
+							<span>
+								{avg} <StarOutlined />
+							</span>
+						}
+					/>
+					<Stat
+						k={
+							<span>
+								4-5 <StarOutlined />
+							</span>
+						}
+						v={`${nPositive} (${round((nPositive / n) * 100, 1)}%)`}
+					/>
+					<Stat
+						k={
+							<span>
+								1-2 <StarOutlined />
+							</span>
+						}
+						v={`${nNegative} (${round((nNegative / n) * 100, 1)}%)`}
+					/>
+					<Stat
+						k={
+							<span>
+								3 <StarOutlined />
+							</span>
+						}
+						v={`${nMixed} (${round((nMixed / n) * 100, 1)}%)`}
+					/>
 				</div>
 			</div>
 		</Card>
--- packages/site/src/pages/Overview.tsx
+++ packages/site/src/pages/Overview.tsx
@@ -1,18 +1,13 @@
 import { data } from '@ghibli-analysis/shared/dist/web';
 import Movie from '../components/Movie';
-import { Col, Row } from 'antd';
 
 const Overview: React.FC = () => {
 	const movies = Object.values(data);
 	return (
-		<div className="px-4">
-			<Row gutter={16}>
-				{movies.map((movie) => (
-					<Col span={4}>
-						<Movie movie={movie} />
-					</Col>
-				))}
-			</Row>
+		<div className="px-4 pb-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-4 gap-x-5">
+			{movies.map((movie) => (
+				<Movie movie={movie} />
+			))}
 		</div>
 	);
 };
