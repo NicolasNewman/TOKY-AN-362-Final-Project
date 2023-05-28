4df88822347ad8df91135231cf3c319f7e2a4063
NicolasNewman <nnewman316@gmail.com>
2023-05-28 11:35:46 +0900

    fixed bug in router path highlighting

--- packages/site/src/components/Route.tsx
+++ packages/site/src/components/Route.tsx
@@ -10,7 +10,7 @@ const Route: React.FC<IProps> = ({ name, path }) => {
 	return (
 		<Link
 			className={`text-xl mx-2 pb-1 hover:text-blue-500 ${
-				loc.pathname === '/home' ? 'border-b-2 hover:border-b-blue-500' : ''
+				loc.pathname === path ? 'border-b-2 hover:border-b-blue-500' : ''
 			}`}
 			to={path}
 		>
