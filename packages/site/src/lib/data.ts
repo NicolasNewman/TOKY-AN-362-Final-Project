95876d8d2e86e14964f6a11d6459b34abeead12b
NicolasNewman <nnewman316@gmail.com>
2023-06-05 08:36:12 +0900

    added explicit path to data source for develop

--- packages/site/src/lib/data.ts
+++ packages/site/src/lib/data.ts
@@ -2,7 +2,7 @@ import { Data, Movie, isBaseData } from '@ghibli-analysis/shared/dist/web';
 import { getEntries } from './object';
 
 const Movies = await (async () => {
-	const data = await (await fetch(`${window.PUBLIC_URL}/data.json`)).json();
+	const data = await (await fetch(`https://nicolasnewman.github.io/${window.PUBLIC_URL}/data.json`)).json();
 	if (isBaseData(data)) {
 		return getEntries(data)
 			.map(([key, movie]) => {
