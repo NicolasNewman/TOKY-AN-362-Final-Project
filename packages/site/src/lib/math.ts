3a3599c3c99332b57fe2bb7a0ccccfc57e1af213
NicolasNewman <nnewman316@gmail.com>
2023-06-08 19:15:33 +0900

    added function for shifting a range of numbers

--- packages/site/src/lib/math.ts
+++ packages/site/src/lib/math.ts
@@ -2,3 +2,12 @@ export const round = (n: number, sigfig: number = 2) => {
 	const d = 10 * sigfig;
 	return Math.round(n * d) / d;
 };
+
+export const shiftRange = (
+	t: number,
+	from: [number, number],
+	to: [number, number],
+	round: (n: number) => number = (n) => n,
+) => {
+	return round(to[0] + ((to[1] - to[0]) / (from[1] - from[0])) * (t - from[0]));
+};
