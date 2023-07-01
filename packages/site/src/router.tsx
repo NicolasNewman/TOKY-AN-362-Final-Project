f4d6ae194dc57318c5512576b3442da87547ff44
NicolasNewman <nnewman316@gmail.com>
2023-07-01 11:25:33 +0900

    removed unused import

--- packages/site/src/router.tsx
+++ packages/site/src/router.tsx
@@ -1,6 +1,5 @@
 import { createHashRouter } from 'react-router-dom';
 import Root from './pages/Root';
-import Home from './pages/Home';
 import Testing from './pages/Testing';
 import NGramExplorer from './pages/NGramExplorer';
 import Overview from './pages/Overview';
